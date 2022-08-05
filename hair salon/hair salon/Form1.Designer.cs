d97ceabccc7edbced1738e38b2d1778dbe071b00
Abel Mesay Gebremeskel <63182609+abel323@users.noreply.github.com>
2022-08-05 23:23:11 +0300

    Add files via upload

---
+++ hair salon/hair salon/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace hair_salon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radLaura = new System.Windows.Forms.RadioButton();
+            this.radSve = new System.Windows.Forms.RadioButton();
+            this.radRon = new System.Windows.Forms.RadioButton();
+            this.radPat = new System.Windows.Forms.RadioButton();
+            this.radJane = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkExtension = new System.Windows.Forms.CheckBox();
+            this.chkHighLights = new System.Windows.Forms.CheckBox();
+            this.chkColour = new System.Windows.Forms.CheckBox();
+            this.chkCut = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radSenior = new System.Windows.Forms.RadioButton();
+            this.radStudent = new System.Windows.Forms.RadioButton();
+            this.radChild = new System.Windows.Forms.RadioButton();
+            this.radStandard = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtVist = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radLaura);
+            this.groupBox1.Controls.Add(this.radSve);
+            this.groupBox1.Controls.Add(this.radRon);
+            this.groupBox1.Controls.Add(this.radPat);
+            this.groupBox1.Controls.Add(this.radJane);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 283);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hair Dresser";
+            // 
+            // radLaura
+            // 
+            this.radLaura.AutoSize = true;
+            this.radLaura.Location = new System.Drawing.Point(17, 228);
+            this.radLaura.Name = "radLaura";
+            this.radLaura.Size = new System.Drawing.Size(171, 29);
+            this.radLaura.TabIndex = 4;
+            this.radLaura.TabStop = true;
+            this.radLaura.Text = "Laura Renkins";
+            this.radLaura.UseVisualStyleBackColor = true;
+            // 
+            // radSve
+            // 
+            this.radSve.AutoSize = true;
+            this.radSve.Location = new System.Drawing.Point(17, 181);
+            this.radSve.Name = "radSve";
+            this.radSve.Size = new System.Drawing.Size(137, 29);
+            this.radSve.TabIndex = 3;
+            this.radSve.TabStop = true;
+            this.radSve.Text = "Sve Pallon";
+            this.radSve.UseVisualStyleBackColor = true;
+            // 
+            // radRon
+            // 
+            this.radRon.AutoSize = true;
+            this.radRon.Location = new System.Drawing.Point(17, 137);
+            this.radRon.Name = "radRon";
+            this.radRon.Size = new System.Drawing.Size(176, 29);
+            this.radRon.TabIndex = 2;
+            this.radRon.TabStop = true;
+            this.radRon.Text = "Ron Chambers";
+            this.radRon.UseVisualStyleBackColor = true;
+            // 
+            // radPat
+            // 
+            this.radPat.AutoSize = true;
+            this.radPat.Location = new System.Drawing.Point(17, 89);
+            this.radPat.Name = "radPat";
+            this.radPat.Size = new System.Drawing.Size(166, 29);
+            this.radPat.TabIndex = 1;
+            this.radPat.TabStop = true;
+            this.radPat.Text = "Pat Johanson";
+            this.radPat.UseVisualStyleBackColor = true;
+            // 
+            // radJane
+            // 
+            this.radJane.AutoSize = true;
+            this.radJane.Location = new System.Drawing.Point(17, 40);
+            this.radJane.Name = "radJane";
+            this.radJane.Size = new System.Drawing.Size(161, 29);
+            this.radJane.TabIndex = 0;
+            this.radJane.TabStop = true;
+            this.radJane.Text = "Jane Samery";
+            this.radJane.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkExtension);
+            this.groupBox2.Controls.Add(this.chkHighLights);
+            this.groupBox2.Controls.Add(this.chkColour);
+            this.groupBox2.Controls.Add(this.chkCut);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(376, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(276, 231);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Services";
+            // 
+            // chkExtension
+            // 
+            this.chkExtension.AutoSize = true;
+            this.chkExtension.Location = new System.Drawing.Point(23, 181);
+            this.chkExtension.Name = "chkExtension";
+            this.chkExtension.Size = new System.Drawing.Size(140, 29);
+            this.chkExtension.TabIndex = 3;
+            this.chkExtension.Text = "Extensions";
+            this.chkExtension.UseVisualStyleBackColor = true;
+            // 
+            // chkHighLights
+            // 
+            this.chkHighLights.AutoSize = true;
+            this.chkHighLights.Location = new System.Drawing.Point(23, 137);
+            this.chkHighLights.Name = "chkHighLights";
+            this.chkHighLights.Size = new System.Drawing.Size(129, 29);
+            this.chkHighLights.TabIndex = 2;
+            this.chkHighLights.Text = "Highlights";
+            this.chkHighLights.UseVisualStyleBackColor = true;
+            // 
+            // chkColour
+            // 
+            this.chkColour.AutoSize = true;
+            this.chkColour.Location = new System.Drawing.Point(23, 89);
+            this.chkColour.Name = "chkColour";
+            this.chkColour.Size = new System.Drawing.Size(98, 29);
+            this.chkColour.TabIndex = 1;
+            this.chkColour.Text = "Colour";
+            this.chkColour.UseVisualStyleBackColor = true;
+            // 
+            // chkCut
+            // 
+            this.chkCut.AutoSize = true;
+            this.chkCut.Location = new System.Drawing.Point(23, 47);
+            this.chkCut.Name = "chkCut";
+            this.chkCut.Size = new System.Drawing.Size(68, 29);
+            this.chkCut.TabIndex = 0;
+            this.chkCut.Text = "Cut";
+            this.chkCut.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radSenior);
+            this.groupBox3.Controls.Add(this.radStudent);
+            this.groupBox3.Controls.Add(this.radChild);
+            this.groupBox3.Controls.Add(this.radStandard);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(13, 330);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(250, 261);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Client Type";
+            // 
+            // radSenior
+            // 
+            this.radSenior.AutoSize = true;
+            this.radSenior.Location = new System.Drawing.Point(17, 200);
+            this.radSenior.Name = "radSenior";
+            this.radSenior.Size = new System.Drawing.Size(189, 29);
+            this.radSenior.TabIndex = 3;
+            this.radSenior.TabStop = true;
+            this.radSenior.Text = "Senior(Over 65)";
+            this.radSenior.UseVisualStyleBackColor = true;
+            // 
+            // radStudent
+            // 
+            this.radStudent.AutoSize = true;
+            this.radStudent.Location = new System.Drawing.Point(17, 151);
+            this.radStudent.Name = "radStudent";
+            this.radStudent.Size = new System.Drawing.Size(108, 29);
+            this.radStudent.TabIndex = 2;
+            this.radStudent.TabStop = true;
+            this.radStudent.Text = "Student";
+            this.radStudent.UseVisualStyleBackColor = true;
+            // 
+            // radChild
+            // 
+            this.radChild.AutoSize = true;
+            this.radChild.Location = new System.Drawing.Point(17, 99);
+            this.radChild.Name = "radChild";
+            this.radChild.Size = new System.Drawing.Size(226, 29);
+            this.radChild.TabIndex = 1;
+            this.radChild.TabStop = true;
+            this.radChild.Text = "Child(12 and under)";
+            this.radChild.UseVisualStyleBackColor = true;
+            // 
+            // radStandard
+            // 
+            this.radStandard.AutoSize = true;
+            this.radStandard.Location = new System.Drawing.Point(17, 51);
+            this.radStandard.Name = "radStandard";
+            this.radStandard.Size = new System.Drawing.Size(187, 29);
+            this.radStandard.TabIndex = 0;
+            this.radStandard.TabStop = true;
+            this.radStandard.Text = "Standard(Adult)";
+            this.radStandard.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtVist);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(376, 330);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(332, 202);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Client Visits";
+            // 
+            // txtVist
+            // 
+            this.txtVist.Location = new System.Drawing.Point(23, 99);
+            this.txtVist.Name = "txtVist";
+            this.txtVist.Size = new System.Drawing.Size(286, 30);
+            this.txtVist.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number Of Client Visits";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(164, 635);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Total: ";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(244, 635);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 25);
+            this.lblTotal.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(52, 693);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(139, 39);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(310, 693);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(139, 39);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(560, 693);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(139, 39);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 762);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Hair Salon";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radLaura;
+        private System.Windows.Forms.RadioButton radSve;
+        private System.Windows.Forms.RadioButton radRon;
+        private System.Windows.Forms.RadioButton radPat;
+        private System.Windows.Forms.RadioButton radJane;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkExtension;
+        private System.Windows.Forms.CheckBox chkHighLights;
+        private System.Windows.Forms.CheckBox chkColour;
+        private System.Windows.Forms.CheckBox chkCut;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radSenior;
+        private System.Windows.Forms.RadioButton radStudent;
+        private System.Windows.Forms.RadioButton radChild;
+        private System.Windows.Forms.RadioButton radStandard;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtVist;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ hair salon/hair salon/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hair_salon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //a method to return value for hair dresser
+        private double hairDresser()
+        {
+            double hairDresser = 0.00;
+            if (radJane.Checked)
+            {
+                hairDresser = 30.00;
+            }
+            else if (radPat.Checked)
+            {
+                hairDresser = 45.00;
+            }
+            else if (radRon.Checked)
+            {
+                hairDresser = 40.00;
+            }
+            else if (radSve.Checked)
+            {
+                hairDresser = 50.00;
+            }
+            else if (radLaura.Checked)
+            {
+                hairDresser = 55.00;
+            }
+            return hairDresser;
+        }
+
+        //a method that will return the price for service
+        private double service()
+        {
+            double serviceType=0.00;
+            if(chkColour.Checked)
+            {
+                serviceType = serviceType + 40;
+            }
+            if(chkCut.Checked)
+            {
+                serviceType = serviceType + 30;
+            }
+            if(chkHighLights.Checked)
+            {
+                serviceType = serviceType + 50;
+            }
+            if(chkExtension.Checked)
+            {
+                serviceType = serviceType + 200;
+            }
+            return serviceType;
+        }
+        //A method that will return the discount price for client type
+        private double clientType()
+        {
+            double discountPercent = 0.00;
+            if (radStandard.Checked)
+            {
+                discountPercent = 0.00;
+            }
+            else if (radChild.Checked)
+            {
+                discountPercent = 0.10;
+            }
+            else if (radStudent.Checked)
+            {
+                discountPercent = 0.05;
+            }
+            else if (radSenior.Checked)
+                discountPercent = 0.15;
+            return discountPercent;
+        }
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Variable that holds total price
+            double total;
+            //declare and assign number of vist to numVist varaible from user input text box
+            int numVisit = int.Parse(txtVist.Text);
+            //get and assign discount for clientType method to local variable discountClient
+            double discountClient = clientType();
+            //Check if the numVisit is postive integer
+            if(numVisit>0)
+            {
+                //Check if the user select atleast one service
+                if (chkCut.Checked || chkColour.Checked || chkExtension.Checked || chkHighLights.Checked)
+                {
+                    //Calculate total with out discount
+                    total = hairDresser() + service();
+                    //conditional statment that calculate discount for number of visits
+                    if (numVisit > 0 && numVisit <= 3)
+                    {
+                        total = total - (total * 0.00);
+                    }
+                    else if (numVisit >= 4 && numVisit <= 8)
+                    {
+                        total = total - (total * 0.05);
+                    }
+                    else if (numVisit >= 9 && numVisit <= 13)
+                    {
+                        total = total - (total * 0.10);
+                    }
+                    else if (numVisit > 14)
+                    {
+                        total = total - (total * 0.15);
+                    }
+                    //calculate discount for client type
+                    total = total - (total * discountClient);
+                    //Display Total Amount
+                    lblTotal.Text = total + "";
+                }
+                else
+                {
+                    //Display a pop up message to notify the user to select at least one service
+                    MessageBox.Show("Please Select At Least One Service!");
+                }
+            }
+            else
+            {
+                //A pop up message to notify to enter a valid number of visit
+                MessageBox.Show("Please enter a valid number of visit!");
+                //Set cursor focut to txtVist textbox
+                txtVist.Focus();
+            }
+            
+        }
+        //btnClear event handler
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtVist.Clear();
+            radJane.Checked = true;
+            radStandard.Checked = true;
+            chkCut.Checked = true;
+            chkColour.Checked = false;
+            chkExtension.Checked = false;
+            chkHighLights.Checked = false;
+            radJane.Focus();
+        }
+    }
+}
